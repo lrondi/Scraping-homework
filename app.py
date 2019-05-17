e1ecfcd9015745a09fdf55840e49cf565695d01b
lrondi <rondissone@gmail.com>
2019-05-17 16:42:48 -0700

    commit

--- app.py
+++ app.py
@@ -5,25 +5,18 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-# mongo = PyMongo(app)
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
 db = client.marsDB
 collection = db.mars
 
-# mars_data = list(db.mars.find())
-
-# Create root/index route to query mongoDB and pass mars data to HTML template to display data
 @app.route('/')
 def index():
     mars_data = db.mars.find_one()
     return render_template("index.html", mars_data=mars_data)
-# def index():
-#     return render_template('index.html', mars_data=mars_data)
 
-# Create route called /scrape
+
 @app.route('/scrape')
 def scrape():
     mars = db.mars
