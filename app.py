464b80f42b9c8f70fee8dc66268621afc9ab7e21
lrondi <rondissone@gmail.com>
2019-05-17 01:52:02 -0700

    commit

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
 import pymongo
 import scrape_mars
 from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+# mongo = PyMongo(app)
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
-db = client.mars_dataDB
-collection = db.mars_data
+db = client.marsDB
+collection = db.mars
 
-mars_data = list(db.mars_data.find())
+# mars_data = list(db.mars.find())
 
 # Create root/index route to query mongoDB and pass mars data to HTML template to display data
 @app.route('/')
 def index():
-    return render_template('index.html', mars_data=mars_data)
+    mars_data = db.mars.find_one()
+    return render_template("index.html", mars_data=mars_data)
+# def index():
+#     return render_template('index.html', mars_data=mars_data)
 
 # Create route called /scrape
 @app.route('/scrape')
 def scrape():
+    mars = db.mars
     mars_data = scrape_mars.scrape()
-    db.mars_data.update(
-        {},
-        mars_data,
-        upsert=True
-    )
-    return redirect('http://localhost:5000/', code=302)
+    db.mars.update({}, mars_data, upsert=True)
+    return redirect('/', code=302)
 
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -2,50 +2,55 @@ def scrape():
     from bs4 import BeautifulSoup
     import requests
     import pandas as pd
+    from splinter import Browser
 
-    news_url = 'https://mars.nasa.gov'
+    mars_data = {}
+    news_url = 'https://mars.nasa.gov/news/'
     news_html = requests.get(news_url)
     news_soup = BeautifulSoup(news_html.text)
-    response = news_soup.find_all('article', class_='news_teaser')
-    news = response[0].find_all('li')
+    news_response = news_soup.find_all('div', class_='image_and_description_container')
     news_titles = []
     news_p = []
-    for i in news:
-        news_titles.append(i.find('h3', class_='title').text)
-        news_p.append(i.find('img',alt=True)['alt'])
-    news_titles = [s.strip() for s in news_titles]
-    n = news_p[1]
-    news_p[1] = n[1:]
-    news_dict = {}
+    for i in range(len(news_response)):
+        news_titles.append(news_response[i].find_all('img', alt=True)[1].get('alt',''))
+        news_p.append(news_response[i].find('div',class_='rollover_description_inner').text.strip())
+    news = []
     for i in range(len(news_titles)):
-        news_subd = {}
-        news_subd['title'] = news_titles[i]
-        news_subd['p'] = news_p[i]
-        news_dict[i] = news_subd
+        news.append({'title':news_titles[i],'p':news_p[i]})
+    mars_data['news'] = news
+
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+    picture_html = browser.html
+    picture_soup = BeautifulSoup(picture_html, 'html.parser')
+    browser.click_link_by_partial_text('FULL IMAGE')
+    part_url = picture_soup.find_all('a', class_='button fancybox')[0].get('data-fancybox-href')
+    image_url = 'https://www.jpl.nasa.gov' + part_url
+    mars_data['image'] = image_url
 
     weather_url =  'https://twitter.com/marswxreport?lang=en'
     weather_html = requests.get(weather_url)
     weather_soup = BeautifulSoup(weather_html.text)
-
-    mars_weather = weather_soup.find('p', class_='TweetTextSize').text
-    mars_weather = mars_weather.replace ('\n', ',')
-    mars_weather = mars_weather[:-27]
+    mars_weather_all = weather_soup.find_all('p', class_='TweetTextSize')
+    mars_weather = mars_weather_all[1].text.replace ('\n', ', ')
+    mars_weather = mars_weather[:-26]
+    mars_data['weather'] = mars_weather
 
     facts_url = 'https://space-facts.com/mars/'
     facts_html = requests.get(facts_url)
     facts_soup = BeautifulSoup(facts_html.text)
-    table = facts_soup.find('table', class_='tablepress')
-    df = pd.read_html(str(table))
+    mars_facts_table = pd.read_html(facts_url)
+    mars_facts = mars_facts_table[0]
+    mars_facts = mars_facts.to_html()
+    mars_data['facts'] = mars_facts
 
     hemisphere_image_url = [{'title':'Cerberus hemisphere', 'img_url':'https://astrogeology.usgs.gov/cache/images/cfa62af2557222a02478f1fcd781d445_cerberus_enhanced.tif_full.jpg'},
     {'title': 'Schiaparelli hemisphere', 'img_url':'https://astrogeology.usgs.gov/cache/images/3cdd1cbf5e0813bba925c9030d13b62e_schiaparelli_enhanced.tif_full.jpg'},
     {'title': 'Syrtis major hemisphere', 'img_url':'https://astrogeology.usgs.gov/cache/images/ae209b4e408bb6c3e67b6af38168cf28_syrtis_major_enhanced.tif_full.jpg'},
     {'title': 'Valles Marineris hemisphere', 'img_url':'https://astrogeology.usgs.gov/cache/images/7cf2da4bf549ed01c17f206327be4db7_valles_marineris_enhanced.tif_full.jpg'}]
-
-    mars_data = {}
-    mars_data['news'] = news_dict
-    mars_data['weather'] = mars_weather
-    mars_data['facts'] = df
-    mars_data['hemispheres'] = hemisphere_image_url
+    mars_data['hemisphere_img'] = hemisphere_image_url
 
     return mars_data
+    
